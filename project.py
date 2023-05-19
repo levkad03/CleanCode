f20e18e3cf3b2cf4e8656630f1dd6d48ca790035
Lev Kadatscky <Lev.Kadaskyi@infiz.khpi.edu.ua>
2023-05-19 17:13:01 +0300

    added new functions, added new constant(parametrized string), simplified some code

--- project.py
+++ project.py
@@ -1,11 +1,83 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+DIAGNOZE = "Ймовірність діагноза %s = "
+
 MULTIPLIER = 0.3333
 POINT_1 = 1
 AGE_20 = 20
 AGE_40 = 40
 AGE_60 = 60
+
+
+def check_age(Age, sympthoms):
+    age_ranges = {
+        20: 1,
+        40: 2,
+        60: 3
+    }
+    for range_start, index in age_ranges.items():
+        if Age < range_start:
+            sympthoms[1, index] += POINT_1
+            break
+    else:
+        sympthoms[1, 4] += POINT_1
+
+    return sympthoms
+
+
+def check_vomit(data, sympthoms):
+    vomit_dict = {
+        'yes': 1,
+        'no': 2
+    }
+
+    sympthoms[2, vomit_dict[data[1]]] += POINT_1
+
+    return sympthoms
+
+
+def check_yellowness(data, sympthoms):
+    yellowness_dict = {
+        'eye': 1,
+        'skin': 2,
+        'no': 3
+    }
+
+    sympthoms[3, yellowness_dict[data[2]]] += POINT_1
+
+    return sympthoms
+
+
+def check_pain(data, sympthoms):
+    # напади болю
+    pain_dict = {
+        'yes': 1,
+        'no': 2
+    }
+    sympthoms[4, pain_dict[data[3]]] += POINT_1
+    return sympthoms
+
+
+def check_liver(data, sympthoms):
+    # збільшення печінка
+    liver_dict = {
+        'yes': 1,
+        'no': 2
+    }
+    sympthoms[5, liver_dict[data[4]]] += POINT_1
+    return sympthoms
+
+
+def check_appetite(data, sympthoms):
+    # апетит
+    appetite_dict = {
+        'yes\n': 1,
+        'no\n': 2
+    }
+    sympthoms[6, appetite_dict[data[5]]] += POINT_1
+    return sympthoms
+
 
 Sympthoms = np.zeros((7, 5))
 # -----------------D1---------------------
@@ -15,7 +87,6 @@ text_file.close()
 D1Size = len(lin)
 Diagnose = D1Size - 1
 
-
 print("'камені жовчних проток' = ", Diagnose)
 
 for i in range(1, D1Size):
@@ -23,51 +94,15 @@ for i in range(1, D1Size):
     # вік
     Age = int(data[0])
 
-    if Age < AGE_20:
-        Sympthoms[1, 1] += POINT_1
-    elif (Age >= AGE_20) and (Age < AGE_40):
-        Sympthoms[1, 2] += POINT_1
-    elif (Age >= AGE_40) and (Age < AGE_60):
-        Sympthoms[1, 3] += POINT_1
-    else:
-        Sympthoms[1, 4] += POINT_1
-
-    # блювота
-    if data[1] == 'yes':
-        Sympthoms[2, 1] += POINT_1
-    else:
-        Sympthoms[2, 2] += POINT_1
-
-    # жовтушність
-    if data[2] == 'eye':
-        Sympthoms[3, 1] += POINT_1
-    elif data[2] == 'skin':
-        Sympthoms[3, 2] += POINT_1
-    else:
-        Sympthoms[3, 3] += POINT_1
-
-    # напади болю
-    if data[3] == 'yes':
-        Sympthoms[4, 1] += POINT_1
-    else:
-        Sympthoms[4, 2] += POINT_1
-
-    # збільшення печінка
-    if data[4] == 'yes':
-        Sympthoms[5, 1] += POINT_1
-    else:
-        Sympthoms[5, 2] += POINT_1
-
-    # апетит
-    if data[5] == 'yes\n':
-        Sympthoms[6, 1] += POINT_1
-    else:
-        Sympthoms[6, 2] += POINT_1
-
+    Sympthoms = check_age(Age, Sympthoms)
+    Sympthoms = check_vomit(data, Sympthoms)
+    Sympthoms = check_yellowness(data, Sympthoms)
+    Sympthoms = check_pain(data, Sympthoms)
+    Sympthoms = check_liver(data, Sympthoms)
+    Sympthoms = check_appetite(data, Sympthoms)
 
 PrK = Sympthoms / Diagnose
 print(PrK)
-
 
 Sympthoms2 = np.zeros((7, 5))
 text_file = open("Ascariasis_var5.csv", "r")
@@ -84,51 +119,15 @@ for i in range(1, D2Size):
     # вік
     Age = int(data2[0])
 
-    if Age < AGE_20:
-        Sympthoms2[1, 1] += POINT_1
-    elif (Age >= AGE_20) and (Age < AGE_40):
-        Sympthoms2[1, 2] += POINT_1
-    elif (Age >= AGE_40) and (Age < AGE_60):
-        Sympthoms2[1, 3] += POINT_1
-    else:
-        Sympthoms2[1, 4] += POINT_1
-
-    # блювота
-    if data2[1] == 'yes':
-        Sympthoms2[2, 1] += POINT_1
-    else:
-        Sympthoms2[2, 2] += POINT_1
-
-    # жовтушність
-    if data2[2] == 'eye':
-        Sympthoms2[3, 1] += POINT_1
-    elif data2[2] == 'skin':
-        Sympthoms2[3, 2] += POINT_1
-    else:
-        Sympthoms2[3, 3] += POINT_1
-
-    # напади болю
-    if data2[3] == 'yes':
-        Sympthoms2[4, 1] += POINT_1
-    else:
-        Sympthoms2[4, 2] += POINT_1
-
-    # збільшення печінка
-    if data2[4] == 'yes':
-        Sympthoms2[5, 1] += POINT_1
-    else:
-        Sympthoms2[5, 2] += POINT_1
-
-    # апетит
-    if data2[5] == 'yes\n':
-        Sympthoms2[6, 1] += POINT_1
-    else:
-        Sympthoms2[6, 2] += POINT_1
+    Sympthoms2 = check_age(Age, Sympthoms2)
+    Sympthoms2 = check_vomit(data2, Sympthoms2)
+    Sympthoms2 = check_yellowness(data2, Sympthoms2)
+    Sympthoms2 = check_pain(data2, Sympthoms2)
+    Sympthoms2 = check_liver(data2, Sympthoms2)
+    Sympthoms2 = check_appetite(data2, Sympthoms2)
 
 PrK2 = Sympthoms2 / Diagnose2
 print(PrK2)
-
-
 
 Sympthoms3 = np.zeros((7, 5))
 text_file = open("Hepatitis_var5.csv", "r")
@@ -145,50 +144,15 @@ for i in range(1, D3Size):
     # вік
     Age = int(data3[0])
 
-    if Age < AGE_20:
-        Sympthoms3[1, 1] += POINT_1
-    elif (Age >= AGE_20) and (Age < AGE_40):
-        Sympthoms3[1, 2] += POINT_1
-    elif (Age >= AGE_40) and (Age < AGE_60):
-        Sympthoms3[1, 3] += POINT_1
-    else:
-        Sympthoms3[1, 4] += POINT_1
-
-    # блювота
-    if data3[1] == 'yes':
-        Sympthoms3[2, 1] += POINT_1
-    else:
-        Sympthoms3[2, 2] += POINT_1
-
-    # жовтушність
-    if data3[2] == 'eye':
-        Sympthoms3[3, 1] += POINT_1
-    elif data3[2] == 'skin':
-        Sympthoms3[3, 2] += POINT_1
-    else:
-        Sympthoms3[3, 3] += POINT_1
-
-    # напади болю
-    if data3[3] == 'yes':
-        Sympthoms3[4, 1] += POINT_1
-    else:
-        Sympthoms3[4, 2] += POINT_1
-
-    # збільшення печінка
-    if data3[4] == 'yes':
-        Sympthoms3[5, 1] += POINT_1
-    else:
-        Sympthoms3[5, 2] += POINT_1
-
-    # апетит
-    if data3[5] == 'yes\n':
-        Sympthoms3[6, 1] += POINT_1
-    else:
-        Sympthoms3[6, 2] += POINT_1
+    Sympthoms3 = check_age(Age, Sympthoms3)
+    Sympthoms3 = check_vomit(data3, Sympthoms3)
+    Sympthoms3 = check_yellowness(data3, Sympthoms3)
+    Sympthoms3 = check_pain(data3, Sympthoms3)
+    Sympthoms3 = check_liver(data3, Sympthoms3)
+    Sympthoms3 = check_appetite(data3, Sympthoms3)
 
 PrK3 = Sympthoms3 / Diagnose3
 print(PrK3)
-
 
 # В этих трех строках происходит вычисление числителя для каждого из трех диагнозов. Каждая строка представляет
 # собой произведение вероятностей различных симптомов, связанных с соответствующим диагнозом. Значение 0.3333
@@ -198,15 +162,15 @@ PrK_D1 = PrK[1, 3] * PrK[2, 1] * PrK[3, 2] * PrK[4, 1] * PrK[5, 1] * PrK[6, 2] *
 PrK_D2 = PrK2[1, 3] * PrK2[2, 1] * PrK2[3, 2] * PrK2[4, 1] * PrK2[5, 1] * PrK2[6, 2] * MULTIPLIER  # числитель
 PrK_D3 = PrK3[1, 3] * PrK3[2, 1] * PrK3[3, 2] * PrK3[4, 1] * PrK3[5, 1] * PrK3[6, 2] * MULTIPLIER  # числитель
 
-PrD1_K = PrK_D1 / (PrK_D1+PrK_D2+PrK_D3)
+PrD1_K = PrK_D1 / (PrK_D1 + PrK_D2 + PrK_D3)
 print("\n")
-print("Ймовірність діагноза 1 = ", round(PrD1_K * 100, 2), '%')
+print(DIAGNOZE % 1, round(PrD1_K * 100, 2), '%')
 
-PrD2_K = PrK_D2 / (PrK_D1+PrK_D2+PrK_D3)
-print("Ймовірність діагноза 2 = ", round(PrD2_K * 100, 2), '%')
+PrD2_K = PrK_D2 / (PrK_D1 + PrK_D2 + PrK_D3)
+print(DIAGNOZE % 2, round(PrD2_K * 100, 2), '%')
 
-PrD3_K = PrK_D3 / (PrK_D1+PrK_D2+PrK_D3)
-print("Ймовірність діагноза 3 = ", round(PrD3_K * 100, 2), '%')
+PrD3_K = PrK_D3 / (PrK_D1 + PrK_D2 + PrK_D3)
+print(DIAGNOZE % 3, round(PrD3_K * 100, 2), '%')
 
 X = [1, 2, 3]
 Y = [round(PrD1_K * 100, 2), round(PrD2_K * 100, 2), round(PrD3_K * 100, 2)]
